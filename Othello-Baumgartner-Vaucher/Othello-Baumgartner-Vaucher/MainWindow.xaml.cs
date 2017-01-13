6330c01da8ed2300d127ae70dcee3d9a797ac0b4
julien-baumgartner <Julien.Baumgartner@he-arc.ch>
2017-01-13 08:23:50 +0100

    initial commit

---
+++ Othello-Baumgartner-Vaucher/Othello-Baumgartner-Vaucher/MainWindow.xaml.cs
@@ -0,0 +1,292 @@
+﻿using OthelloConsole;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Othello_Baumgartner_Vaucher
+{
+
+
+
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, IPlayable
+    {
+        
+        private MyButton[,] listButtons = new MyButton[8, 8];
+        private bool isWhite = true;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    listButtons[i, j] = new MyButton(this, i, j);
+                    Grid.SetRow(listButtons[i, j], i);
+                    Grid.SetColumn(listButtons[i, j], j);
+                    
+                    mainGrid.Children.Add(listButtons[i, j]);
+                }
+            }
+
+            listButtons[3, 3].Type = 1;
+            listButtons[4, 3].Type = 2;
+            listButtons[3, 4].Type = 2;
+            listButtons[4, 4].Type = 1;
+        }
+
+        public void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            MyButton button = (MyButton)sender;
+
+            play(button.Row, button.Col);
+            
+
+        }
+
+        public void play(int x, int y)
+        {
+            if(((IPlayable)this).playMove(x, y, isWhite))
+                isWhite = !isWhite;
+        }
+
+        int IPlayable.getBlackScore()
+        {
+            throw new NotImplementedException();
+        }
+
+        Tuple<char, int> IPlayable.getNextMove(int[,] game, int level, bool whiteTurn)
+        {
+            throw new NotImplementedException();
+        }
+
+        int IPlayable.getWhiteScore()
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IPlayable.isPlayable(int column, int line, bool isWhite)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IPlayable.playMove(int column, int line, bool isWhite)
+        {
+            if (listButtons[column, line].Type == 0)
+            {
+                if (isWhite)
+                    listButtons[column, line].Type = 1;
+                else
+                    listButtons[column, line].Type = 2;
+
+            }
+            List<MyButton> capturedTiles = new List<MyButton>();
+            int actualPlayer, otherPlayer;
+            if (isWhite)
+            {
+                actualPlayer = 1;
+                otherPlayer = 2;
+            }else
+            {
+                actualPlayer = 2;
+                otherPlayer = 1;
+            }
+
+
+            /**************************************
+             *                - y                 *
+             **************************************/
+            for (int y = line - 1; y >= 0; y--)
+            {
+                if (listButtons[column, y].Type == otherPlayer)
+                {
+                    capturedTiles.Add(listButtons[column, y]);
+                } else
+                {
+                    if (listButtons[column, y].Type == actualPlayer)
+                    {
+                        foreach (MyButton tile in capturedTiles)
+                        {
+                            tile.Type = actualPlayer;
+                        }
+                    }
+                    break;
+                }
+            }
+            capturedTiles.Clear();
+            /**************************************
+            *                + y                  *
+            **************************************/
+            for (int y = line + 1; y < 8; y++)
+            {
+                if (listButtons[column, y].Type == otherPlayer)
+                {
+                    capturedTiles.Add(listButtons[column, y]);
+                }
+                else
+                {
+                    if (listButtons[column, y].Type == actualPlayer)
+                    {
+                        foreach (MyButton tile in capturedTiles)
+                        {
+                            tile.Type = actualPlayer;
+                        }
+                    }
+                    break;
+                }
+            }
+            capturedTiles.Clear();
+            /**************************************
+            *                - x                 *
+            **************************************/
+            for (int x = column - 1; x >= 0; x--)
+            {
+                if (listButtons[x, line].Type == otherPlayer)
+                {
+                    capturedTiles.Add(listButtons[x, line]);
+                }
+                else
+                {
+                    if (listButtons[x, line].Type == actualPlayer)
+                    {
+                        foreach (MyButton tile in capturedTiles)
+                        {
+                            tile.Type = actualPlayer;
+                        }
+                    }
+                    break;
+                }
+            }
+            capturedTiles.Clear();
+            /**************************************
+            *                + x                 *
+            **************************************/
+            for (int x = column + 1; x < 8; x++)
+            {
+                if (listButtons[x, line].Type == otherPlayer)
+                {
+                    capturedTiles.Add(listButtons[x, line]);
+                }
+                else
+                {
+                    if (listButtons[x, line].Type == actualPlayer)
+                    {
+                        foreach (MyButton tile in capturedTiles)
+                        {
+                            tile.Type = actualPlayer;
+                        }
+                    }
+                    break;
+                }
+            }
+            capturedTiles.Clear();
+
+            /**************************************
+            *               - y + x               *
+            **************************************/
+            for (int x = column + 1,  y = line - 1; x < 8 && y >= 0; x++, y--)
+            {
+                if (listButtons[x, y].Type == otherPlayer)
+                {
+                    capturedTiles.Add(listButtons[x, y]);
+                }
+                else
+                {
+                    if (listButtons[x, y].Type == actualPlayer)
+                    {
+                        foreach (MyButton tile in capturedTiles)
+                        {
+                            tile.Type = actualPlayer;
+                        }
+                    }
+                    break;
+                }
+            }
+            capturedTiles.Clear();
+
+            /**************************************
+           *               + y + x               *
+           **************************************/
+            for (int x = column + 1, y = line + 1; x < 8 && y < 8; x++, y++)
+            {
+                if (listButtons[x, y].Type == otherPlayer)
+                {
+                    capturedTiles.Add(listButtons[x, y]);
+                }
+                else
+                {
+                    if (listButtons[x, y].Type == actualPlayer)
+                    {
+                        foreach (MyButton tile in capturedTiles)
+                        {
+                            tile.Type = actualPlayer;
+                        }
+                    }
+                    break;
+                }
+            }
+            capturedTiles.Clear();
+
+            /**************************************
+           *               + y - x               *
+           **************************************/
+            for (int x = column - 1, y = line + 1; y < 8 && x >= 0; y++, x--)
+            {
+                if (listButtons[x, y].Type == otherPlayer)
+                {
+                    capturedTiles.Add(listButtons[x, y]);
+                }
+                else
+                {
+                    if (listButtons[x, y].Type == actualPlayer)
+                    {
+                        foreach (MyButton tile in capturedTiles)
+                        {
+                            tile.Type = actualPlayer;
+                        }
+                    }
+                    break;
+                }
+            }
+            capturedTiles.Clear();
+
+            /**************************************
+           *               - y - x               *
+           **************************************/
+            for (int x = column - 1, y = line - 1; x >= 0 && y >= 0; x--, y--)
+            {
+                if (listButtons[x, y].Type == otherPlayer)
+                {
+                    capturedTiles.Add(listButtons[x, y]);
+                }
+                else
+                {
+                    if (listButtons[x, y].Type == actualPlayer)
+                    {
+                        foreach (MyButton tile in capturedTiles)
+                        {
+                            tile.Type = actualPlayer;
+                        }
+                    }
+                    break;
+                }
+            }
+            capturedTiles.Clear();
+            return true;
+        }
+    }
+}
---
+++ Othello-Baumgartner-Vaucher/Othello-Baumgartner-Vaucher/MyButton.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Othello_Baumgartner_Vaucher
+{
+    class MyButton : Button
+    {
+
+        private MainWindow parent;
+        private LinearGradientBrush defaultBackground;
+        private LinearGradientBrush playableBackground;
+        private int type = 0;
+        public int Type
+        {
+            get { return type; }
+            set
+            {
+                type = value;
+                if(type == 1)
+                {
+                    Background = new LinearGradientBrush(Colors.White, Colors.White, 90);
+                }
+                else if (type == 2)
+                {
+                    Background = new LinearGradientBrush(Colors.Black, Colors.Black, 90);
+                }
+            }
+        }
+
+        private int row;
+        public int Row
+        {
+            get { return row; }
+        }
+
+        private int col;
+        public int Col
+        {
+            get { return col; }
+        }
+
+        public MyButton(MainWindow parent, int row, int col)
+        {
+            this.parent = parent;
+            this.row = row;
+            this.col = col;
+            defaultBackground = new LinearGradientBrush(Colors.GreenYellow, Colors.Green, 90);
+            playableBackground = new LinearGradientBrush(Colors.Azure, Colors.Aqua, 90);
+            this.Background = defaultBackground;
+            this.Click += parent.myButton_Click;
+        }
+
+        public void changeBackground(bool isPlayable)
+        {
+            if (isPlayable)
+            {
+                this.Background = playableBackground;
+            }else
+            {
+                this.Background = defaultBackground;
+            }
+        }
+
+    }
+}
